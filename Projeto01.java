b7414fca5ccd661be1e84da67041562268fdea91
Leonardo Santos <leonardorenatobastos@sempreceub.com>
2024-10-13 18:52:02 -0300

    função de multiplicação

--- Projeto01.java
+++ Projeto01.java
@@ -3,21 +3,36 @@ package projeto;
 import java.util.Scanner;
 
 public class Projeto01 {
-
-	 public static void main(String[] args) {
+	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
+	        System.out.println("Escolha  a operação:");
+	        System.out.println("1. Soma");
+	        System.out.println("2. Multiplicação");
+	        int opcao = scanner.nextInt();
+
 	        System.out.print("Digite o primeiro número: ");
-	        int numero1 = scanner.nextInt();
+	        double numero1 = scanner.nextDouble();
 
-	        System.out.print("Digite o segundo número: ");
-	        int numero2 = scanner.nextInt();
+	        System.out.print("Digite o segundo  número: ");
+	        double numero2 = scanner.nextDouble(); 
 
-	        int soma = numero1 + numero2;
 
-	        System.out.println("A soma dos números é:  " + soma);
+	        double resultado;
+
+	        switch (opcao) {
+	            case 1:
+	                resultado = numero1 + numero2;
+	                System.out.println("A soma dos números é: " + resultado);
+	                break;
+	            case 2:
+	                resultado = numero1 * numero2;
+	                System.out.println("A multiplicação dos números é: " + resultado);
+	                break;
+	            default:
+	                System.out.println("Opção inválida.");
+	        }
 
 	        scanner.close();
-
 	    }
 	}
